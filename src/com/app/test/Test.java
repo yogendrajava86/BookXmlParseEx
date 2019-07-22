1424f4e3b144b5dcd399d1bcc604f5c2c815d7a1
yogendradixit86 <dixitshloke7@gmail.com>
2019-07-22 12:44:16 +0530

    Added XML Validation based on XSD

--- src/com/app/test/Test.java
+++ src/com/app/test/Test.java
@@ -2,8 +2,13 @@ package com.app.test;
 
 import java.io.File;
 
+import javax.xml.XMLConstants;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
 
 import com.app.handler.BookHandler;
 
@@ -12,6 +17,26 @@ public class Test {
 	public static void main(String[] args) throws Exception {
 		SAXParserFactory factory=SAXParserFactory.newInstance();
 		SAXParser parser=factory.newSAXParser();
-		parser.parse(new File("Book.xml"), new BookHandler());
+		
+		boolean validStatus=validateXmlFromXsd("Book.xsd","Book.xml");
+		if(validStatus) {
+			parser.parse(new File("Book.xml"), new BookHandler());
+		}else {
+			System.out.println("Sorry, provided XML not valid.");
+		}
+		
+		
+	}
+	public static boolean validateXmlFromXsd(String xsdPath,String xmlPath) {
+		try {
+		SchemaFactory sf=SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+		Schema schema=sf.newSchema(new File(xsdPath));
+		Validator validator=schema.newValidator();
+		validator.validate(new StreamSource(xmlPath));
+		}catch(Exception ex) {
+			//ex.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 }
